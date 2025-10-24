befc90bcf8b0f5cdbf1ba31e7cbd08b59f8812cd
Bruno Vieira <124227458+3212-2829@users.noreply.github.com>
2025-10-23 23:42:59 -0300

    Criação e entidade Ninja e adição da dependecia JPA

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,52 @@
+package dev.java10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+// Atra´ves da dependencia JPA - Faço da classe uma entidade (JPA JAVA PERSISTENCE API)
+@Entity
+//Crio tabela
+@Table(name = "tb_cadastro_de_ninja")
+public class NinjaModel {
+
+    @Id
+//    Valores de incrementação
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
