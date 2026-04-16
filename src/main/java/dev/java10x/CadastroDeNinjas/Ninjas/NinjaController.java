aeefa6ea151d16d3c903315560f942c3e2e52c8b
Bruno Vieira <124227458+3212-2829@users.noreply.github.com>
2026-04-16 15:14:56 -0300

    feat: Adiciona End Point - NinjaController

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,15 +1,44 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
 
     @GetMapping("/boas-vindas")
-    public String boasVindas(){
+    public String boasVindas() {
         return "Essa é minha primeira rota";
     }
+
+    // Adicinar Ninja
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+
+    // Mostar todos os Ninjas
+    @GetMapping("/all")
+    public String mostrarTodos(){
+        return "Mostrar todos os ninjas";
+    }
+
+    // Procurar Ninja por ID
+    @GetMapping("/allID")
+    public String mostrarPorId(){
+        return "Mostrar ninjas por ID";
+    }
+
+    // Alterar dados dos Ninjas
+    @PutMapping("/alterarID")
+    public String alterarPorId(){
+        return "Alterar ninja";
+    }
+    // Deletar Ninja
+        @DeleteMapping("/deletarID")
+    public String deletarNinjaID(){
+        return("Ninja Deletado");
+    }
+
 }
+
